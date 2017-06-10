c458f29840a421036d5bc1d9b87ae0eb33fa6ec4
shtnkgm <nakagami.shota@gmail.com>
2017-06-11 01:45:14 +0900

    add xcode project files

---
+++ ViewLifecycleSample/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  ViewLifecycleSample
+//
+//  Created by Shota Nakagami on 2017/06/11.
+//  Copyright © 2017年 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+
+
+}
+
---
+++ ViewLifecycleSample/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ViewLifecycleSample
+//
+//  Created by Shota Nakagami on 2017/06/11.
+//  Copyright © 2017年 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
